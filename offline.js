e4809cadd56476969fa846d5b1a3f2f10bcb5dce
aura520 <aurajuliap6@gmail.com>
2025-12-15 15:55:33 +0700

    Add game files

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1765788560,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/karakter_utama-sheet0.png",
+		"images/karakter_utama-sheet1.png",
+		"images/karakter_utama-sheet2.png",
+		"images/balok-sheet0.png",
+		"images/koin-sheet0.png",
+		"images/camera-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
